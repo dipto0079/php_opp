bea0989eaa607981517139ce1a12af2307f22919
Sudipto Biswas <sudiptobiswas423@gmail.com>
2020-11-06 12:22:05 +0600

    Interfaces

---
+++ oop/interfaces.php
@@ -0,0 +1,22 @@
+<?php
+interface parent1{
+    function calc($a,$b);
+}
+interface parent2{
+    function sub($c,$d);
+}
+class childClass implements parent1 ,parent2{
+    public function calc($a, $b)
+    {
+        echo $a+$b;
+    }
+    public function sub($c, $d)
+    {
+        echo $c-$d;
+    }
+}
+$test = new childClass();
+
+$test->calc(20,35);
+echo "<br>";
+$test->sub(40,35);

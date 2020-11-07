8b6f76e3ad673c20e93b0220cb0a2ee69a1ba04a
Sudipto Biswas <sudiptobiswas423@gmail.com>
2020-11-07 12:54:16 +0600

    Traits Method Overriding

---
+++ oop/traits-method-overriding.php
@@ -0,0 +1,25 @@
+<?php
+trait hello{
+    protected function sayhello(){
+        echo"Hello from Hello Trait.\n";
+    }
+}
+class base{
+    use hello{
+        hello::sayhello as public;
+    }
+}
+/*trait hi{
+    public function sayhello(){
+        echo"Hello from Hi Trait.\n";
+    }
+}
+class base{
+    use hello,hi{
+        hello::sayhello insteadof hi;
+        hi::sayhello as newhello;
+    }
+}*/
+$test=new base();
+$test->sayhello();
+//$test->newhello();

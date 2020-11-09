923d9fd96457b01def379ffddf8794345428c54d
Sudipto Biswas <sudiptobiswas423@gmail.com>
2020-11-09 09:21:24 +0600

    call Method

---
+++ oop/callMethod.php
@@ -0,0 +1,11 @@
+<?php
+/*class student{
+    private $first_name;
+    private $last_name;
+    private function setName($fname,$lname){
+        $this->first_name=$fname;
+        $this->last_name=$lname;
+    }
+}
+$test=new student();
+$test->setName("dip","roy");*/
